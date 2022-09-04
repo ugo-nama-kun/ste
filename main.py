3c30f331f2b9d1e999cf5c13218d14aac9c77567
Naoto Yoshida <yossy0157@gmail.com>
2022-09-05 02:06:35 +0900

    strainght-through estimator plut entropy

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import numpy as np
 import torch
+
+import matplotlib
+matplotlib.rcParams['backend'] = 'TkAgg'
 import matplotlib.pyplot as plt
 
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.distributions import Bernoulli
 
 from nist import NistHandle
@@ -16,77 +20,89 @@ class StochasticModel(nn.Module):
     def __init__(self):
         super(StochasticModel, self).__init__()
 
-        self.fc1 = nn.Linear(1, 1)
-        self.fc2 = nn.Linear(1, 64)
+        self.fc1 = nn.Linear(2, 1)
+        self.fc2 = nn.Linear(1, 100)
+        self.fc3 = nn.Linear(100, 64)
 
     def forward(self, x):
-        h = self.fc1(x)
-        h = torch.sigmoid_(h)
+        h = F.one_hot(x, num_classes=2).float()
+        h = self.fc1(h)
+        p = torch.sigmoid_(h)
 
-        dist = Bernoulli(probs=h)
+        dist = Bernoulli(probs=p)
         z = dist.sample()
+        h_ = z.detach() + p - p.detach()  # straight-through gradient computation
 
-        h_ = z.detach() + h - h.detach()
-        y = torch.sigmoid_(self.fc2(h_))
-        return y
+        h = torch.relu_(self.fc2(h_))
+        y = torch.sigmoid_(self.fc3(h))
+        return y, p, dist.entropy()
 
 
 def get_image(model):
     n_img = 16
 
-    y0 = model(torch.zeros((n_img, 1))).numpy()
-    y1 = model(torch.ones((n_img, 1))).numpy()
+    y0, p0, _ = model(torch.zeros((n_img,), dtype=torch.long))
+    y1, p1, _ = model(torch.ones((n_img,), dtype=torch.long))
 
-    im0 = np.vstack([y0[i].reshape(8, 8) for i in range(n_img)])
-    im1 = np.vstack([y0[i].reshape(8, 8) for i in range(n_img)])
+    y0 = y0.detach().numpy()
+    y1 = y1.detach().numpy()
+
+    print(f"   x=0: p={p0.detach().mean()}, x=1: p={p1.detach().mean()}")
+
+    im0 = np.hstack([y0[i].reshape(8, 8) for i in range(n_img)])
+    im1 = np.hstack([y1[i].reshape(8, 8) for i in range(n_img)])
 
     plt.clf()
     plt.subplot(211)
+    plt.title("$x = 0$")
     plt.imshow(im0, "gray")
+
     plt.subplot(212)
+    plt.title("$x = 1$")
     plt.imshow(im1, "gray")
-    plt.pause(3)
+
+    plt.pause(0.0001)
 
 
 def train():
-    fig = plt.figure()
+    plt.figure()
 
     model = StochasticModel()
     optimizer = torch.optim.Adam(model.parameters())
     loss_mse = nn.MSELoss()
 
-    n_iteration = 30
+    n_iteration = 5000
     mini_batch_size = 32
 
     for n in range(n_iteration):
         total_loss = 0
 
-        for _ in range(6):
+        for _ in range(10):
             # Data
-            x = torch.randint(0, 2, (mini_batch_size, 1))
-            y_t = np.zeros(mini_batch_size, 64)
+            x = torch.randint(0, 2, (mini_batch_size,))
+            y_t = np.zeros((mini_batch_size, 64))
 
             for i in range(mini_batch_size):
                 r = np.random.rand()
 
                 if x[i] == 0:
-                    if r < 0.8:
-                        img = nh.get(target=0)
+                    if r < 0.4:
+                        img = nh.get(target=3)
                     else:
-                        img = nh.get(target=1)
+                        img = nh.get(target=4)
                 else:
-                    if r < 0.2:
-                        img = nh.get(target=0)
+                    if r < 0.6:
+                        img = nh.get(target=3)
                     else:
-                        img = nh.get(target=1)
+                        img = nh.get(target=4)
 
                 y_t[i] = img
 
             y_t = torch.as_tensor(y_t / 16., dtype=torch.float32)
 
-            y_predict = model(x)
+            y_pred, p_pred, entropy = model(x)
 
-            loss = loss_mse(y_predict, y_t)
+            loss = loss_mse(y_pred, y_t) - 0.01 * entropy.mean()
 
             total_loss += loss.item()
 
@@ -95,13 +111,17 @@ def train():
             loss.backward()
             optimizer.step()
 
-        print(f"{n}-th iteration: LOSS {total_loss}")
-        get_image(model)
+        if n % 20 == 0:
+            print(f"{n}-th iteration: LOSS {total_loss}: Entropy: {entropy.mean().item()}")
+            get_image(model)
 
     print("Finish")
+    plt.show()
 
 
 if __name__ == '__main__':
-    dist = Bernoulli(probs=[0.5])
-    for i in range(10):
-        print(dist.sample(sample_shape=(4,)))
+    # dist = Bernoulli(probs=torch.tensor([0.5]))
+    # for i in range(10):
+    #     print(dist.sample(sample_shape=(4, 1)))
+
+    train()
